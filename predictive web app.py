a83d1513789c4a6560a377e222e0ca263bc8f900
Sadini Siriwardana <88416199+IT20155070@users.noreply.github.com>
2022-11-02 00:27:57 +0530

    new

---
+++ predictive web app.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.pexels.com/photos/261181/pexels-photo-261181.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+background-size: 120%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+# loading the saved model
+loaded_model = pickle.load(open('C:/Users/sadini/Desktop/FDM/trained_model.sav', 'rb'))
+
+# creating a function for Prediction
+
+def diabetes_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'Resort Hotel'
+    else:
+      return 'City Hotel'
+  
+    
+  
+def main():
+    
+    
+    # giving a title
+    #st.title('Hotel Prediction Web App')
+    st.markdown("<h1 style='text-align: center; color: white;'>Hotel Prediction Web App</h1>", unsafe_allow_html=True)
+    
+    # getting the input data from the user
+    
+    form = st.form("Form 1")
+    #meal_list = ['Select...','BB','FB','HB','SC','Undefined']
+    meal_list = ['',0,1,2,3,4]
+    meal = form.selectbox('Select Meal',meal_list)
+    
+    #market_segment = ['Select...','Direct','Corporate','Online TA','Offline TA/TO','Complementary','Groups','Aviation']
+    market_segment = ['',0,1,2,3,4,5,6]
+    market_segment = form.selectbox('Select Marcket Segment',market_segment)
+    
+    #reserved_room_type_list = ['Select...','C','A','D','E','G','F','H','L','B']
+    reserved_room_type_list = ['',1,2,3,4,5,6,7,8,9]
+    reserved_room_type_list = form.selectbox('Select Room Type',reserved_room_type_list)
+    
+    col1,col2 = form.columns(2)
+    stays_in_weekend_nights = col1.text_input('Weekend Nights')
+    
+    stays_in_week_nights = col2.text_input('Weekday Nights')
+    
+    col3,col4,col5 = form.columns(3)
+    adults = col3.text_input('No of Adults')   
+    children = col4.text_input('No of Childrens')
+    babies = col5.text_input('No of Babies')
+    
+    col6,col7 = form.columns(2)
+    adr= col6.text_input('adr')
+    
+    required_car_parking_spaces = col7.text_input('required_car_parking_spaces')
+    
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    btnsubmit = form.form_submit_button('Hotel Result')
+    
+    if btnsubmit:
+        if stays_in_weekend_nights == '':
+            col1.error("Pleace enter no of weekend nights.")
+            st.stop()
+    
+    if btnsubmit:
+        if stays_in_week_nights == '':
+            col2.error("Pleace enter no of weekday nights.")
+            st.stop()
+            
+    if btnsubmit:
+        if adults == '':
+            col3.error("Pleace enter no of adults.")
+            st.stop()
+            
+    if btnsubmit:
+        if children == '':
+            col4.error("Pleace enter no of childrens.")
+            st.stop()
+    
+    if btnsubmit:
+        if babies == '':
+            col5.error("Pleace enter no of babies.")
+            st.stop()
+            
+    if btnsubmit:
+        if adr == '':
+            col6.error("Pleace enter adr.")
+            st.stop()
+    
+    if btnsubmit:
+        if required_car_parking_spaces == '':
+            col7.error("Pleace enter required car parking spaces.")
+            st.stop()
+            
+    if btnsubmit:
+        try:
+            int(stays_in_weekend_nights)
+            
+        except ValueError:
+           col1.error("Pleace enter numaric values.")
+           st.stop()
+    
+    if btnsubmit:
+        try:
+            int(stays_in_week_nights)
+            
+        except ValueError:
+           col2.error("Pleace enter numaric values.")
+           st.stop()
+           
+    if btnsubmit:
+        try:
+            int(adults)
+            
+        except ValueError:
+           col3.error("Pleace enter numaric values.")
+           st.stop()
+           
+    if btnsubmit:
+        try:
+            int(children)
+            
+        except ValueError:
+           col4.error("Pleace enter numaric values.")
+           st.stop()
+    
+    if btnsubmit:
+        try:
+            int(babies)
+            
+        except ValueError:
+           col5.error("Pleace enter numaric values.")
+           st.stop()
+           
+    if btnsubmit:
+        diagnosis = diabetes_prediction([meal, market_segment, reserved_room_type_list, stays_in_weekend_nights, stays_in_week_nights, adults, children, babies, adr, required_car_parking_spaces])
+        
+        
+    form.success(diagnosis)
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ predictive_system.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 02:30:20 2022
+
+@author: sadini
+"""
+
+import numpy as np
+import pickle
+
+loaded_model = pickle.load(open('C:/Users/sadini/Desktop/FDM/trained_model.sav', 'rb'))
+
+input_data = (0,1,3,0,2,2,0,2,3,1)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('Resort Hotel')
+else:
+  print('City Hotel')
